7de364ce8eace034670bc6c102cac65c339adb0c
vaik-info <info@vaikobo.com>
2023-03-13 10:25:21 +0900

    first commit

--- models/mobile_net_v2_model.py
+++ models/mobile_net_v2_model.py
@@ -1,10 +1,10 @@
-from torchvision import models
 import torch.nn as nn
+import timm
 
 class MobileNetV2Model(nn.Module):
     def __init__(self, class_num):
         super(MobileNetV2Model, self).__init__()
-        self.mobile_net_v2 = models.mobilenet_v2(pretrained=True)
+        self.mobile_net_v2 = timm.create_model('mobilenetv2_100', pretrained=True)
         self.mobile_net_v2.classifier = nn.Sequential(
             nn.Dropout(p=0.2, inplace=False),
             nn.Linear(in_features=1280, out_features=class_num, bias=True)
--- train.py
+++ train.py
@@ -1,12 +1,19 @@
 import argparse
 import os
 import multiprocessing
+import tqdm
+from datetime import datetime
+import pytz
 
 import torch
 import torchvision.transforms as T
 from torch.utils.data import DataLoader
 import torch.optim as optim
 import torch.nn as nn
+import torchmetrics
+from timm.scheduler import StepLRScheduler
+from torch.utils.tensorboard import SummaryWriter
+
 
 from data import classification_dataset
 from models import mobile_net_v2_model
@@ -14,14 +21,16 @@ from models import mobile_net_v2_model
 
 def train(train_input_dir_path, valid_input_dir_path, classes_txt_path, epochs, step_size, batch_size,
           test_max_sample, image_size, output_dir_path):
+    # read classes
     with open(classes_txt_path, 'r') as f:
         classes = f.readlines()
     classes = [label.strip() for label in classes]
 
     # train dataset
-    train_dataset = classification_dataset.MNISTImageDataset(train_input_dir_path, classes, step_size, image_size,
-                                                             T.Compose([T.RandomRotation(degrees=[-7.5, 7.5]),
-                                                                        T.RandomErasing(p=0.1)]))
+    train_dataset = classification_dataset.MNISTImageDataset(train_input_dir_path, classes, batch_size * step_size,
+                                                             image_size,
+                                                             T.Compose([T.RandomRotation(degrees=[-2.5, 2.5]),
+                                                                        T.RandomErasing(p=0.05)]))
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
                                   num_workers=multiprocessing.cpu_count() // 4)
 
@@ -30,32 +39,61 @@ def train(train_input_dir_path, valid_input_dir_path, classes_txt_path, epochs, 
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True,
                                  num_workers=multiprocessing.cpu_count() // 4)
 
+    # prepare device
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
     # prepare model
     model = mobile_net_v2_model.MobileNetV2Model(len(classes))
-
-    # prepare loss & optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.001)
-
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     model.to(device)
-    # train
-    for epoch in range(epochs):  # loop over the dataset multiple times
 
-        running_loss = 0.0
-        for i, data in enumerate(train_dataloader):
-            inputs, labels = data[0].to(device), data[1].to(device)
+    # prepare loss
+    criterion = nn.CrossEntropyLoss()
+
+    # prepare optimizer
+    optimizer = optim.SGD(model.parameters(), lr=0.001)
+    scheduler = StepLRScheduler(optimizer,
+                                decay_t=epochs//4,
+                                decay_rate=0.5,
+                                t_in_epochs=True)
+
+    # prepare metrics
+    train_acc = torchmetrics.Accuracy(task='multiclass', num_classes=len(classes))
+    val_acc = torchmetrics.Accuracy(task='multiclass', num_classes=len(classes))
+
+    # prepare logs
+    save_model_dir_path = os.path.join(output_dir_path,
+                                       f'{datetime.now(pytz.timezone("Asia/Tokyo")).strftime("%Y-%m-%d-%H-%M-%S")}')
+    os.makedirs(save_model_dir_path, exist_ok=True)
+    summary_writer = SummaryWriter(os.path.join(save_model_dir_path, 'logs'))
+
+    # train
+    for epoch in tqdm.tqdm(range(epochs), desc='epoch'):
+        model.train()
+        train_acc.reset(), val_acc.reset()
+
+        for x, y in tqdm.tqdm(train_dataloader, desc='train'):
             optimizer.zero_grad()
 
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
+            x, y = x.to(device), y.to(device)
+            y_pred = model(x)
+            loss = criterion(y_pred, y)
             loss.backward()
             optimizer.step()
 
-            running_loss += loss.item()
-            if i % 100 == 99:
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 100:.3f}')
-                running_loss = 0.0
+            train_acc(torch.argmax(y_pred, -1).cpu(), y.cpu())
+
+        model.eval()
+        for x, y in tqdm.tqdm(test_dataloader, desc='test'):
+            with torch.no_grad():
+                x, y = x.to(device), y.to(device)
+                y_pred = model(x)
+            val_acc(torch.argmax(y_pred, -1).cpu(), y.cpu())
+
+        print(f"Epoch{epoch+1}, TrainAcc:{train_acc.compute()}, ValAcc:{val_acc.compute()}")
+        torch.save(model.state_dict(), os.path.join(save_model_dir_path, f'epoch-{epoch}_step-{step_size}_batch-{batch_size}_val_acc-{train_acc.compute():.4f}'))
+        summary_writer.add_scalar("Train Accuracy", train_acc.compute(), epoch+1)
+        summary_writer.add_scalar("Validation Accuracy", val_acc.compute(), epoch+1)
+        scheduler.step(epoch+1)
 
 
 if __name__ == '__main__':
@@ -64,12 +102,12 @@ if __name__ == '__main__':
     parser.add_argument('--valid_input_dir_path', type=str, default='~/.vaik-mnist-classification-dataset/valid')
     parser.add_argument('--classes_txt_path', type=str, default='~/.vaik-mnist-classification-dataset/classes.txt')
     parser.add_argument('--epochs', type=int, default=10)
-    parser.add_argument('--step_size', type=int, default=8000)
-    parser.add_argument('--batch_size', type=int, default=8)
-    parser.add_argument('--test_max_sample', type=int, default=100)
+    parser.add_argument('--step_size', type=int, default=1000)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--test_max_sample', type=int, default=200)
     parser.add_argument('--image_height', type=int, default=224)
     parser.add_argument('--image_width', type=int, default=224)
-    parser.add_argument('--output_dir_path', type=str, default='~/output_model')
+    parser.add_argument('--output_dir_path', type=str, default='~/.vaik-classification-pth-trainer/output_model')
     args = parser.parse_args()
 
     args.train_input_dir_path = os.path.expanduser(args.train_input_dir_path)
